1113b09b5c03d3544e9545724e5f5b52dea48ce8
Travis Lang <tlang50@icloud.com>
2018-12-12 13:03:52 -0600

    initial commit

---
+++ src/components/App/App.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import axios from 'axios';
+
+class App extends Component {
+  state = {
+    newElement: '',
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      newElement: event.target.value,
+    });
+  }
+
+  getElements = () => {
+    axios.get('/api/element').then(response => {
+      this.props.dispatch({ type: 'SET_ELEMENTS', payload: response.data });
+  })
+  .catch(error => {
+      console.log('error with element get request', error);
+  });
+  }
+
+  componentDidMount() {
+    this.getElements();
+  }
+
+  handleClick = () => {
+    axios.post('/api/element', this.state).then(() => {
+        this.getElements();
+        this.setState({
+          newElement: '',
+      });
+    })
+    .catch(error => {
+        console.log('error with element get request', error);
+    });
+    
+}
+
+  render() {
+    return (
+      <div>
+        <button onClick={() => this.props.dispatch({ type: 'BUTTON_ONE' })}>Button One</button>
+        <button onClick={() => this.props.dispatch({ type: 'BUTTON_TWO' })}>Button Two</button>
+        <input value={this.state.newElement} onChange={this.handleChange} />
+        <button onClick={this.handleClick}>Add Element</button>
+        <pre>{JSON.stringify(this.props.reduxState)}</pre>
+      </div>
+    );
+  }
+}
+
+const mapReduxStateToProps = reduxState => ({
+    reduxState
+});
+
+export default connect(mapReduxStateToProps)(App);
